e9b21c664f8d25f365a56c988c3f381347bc9c43
Sirocco1106 <sirocco1106@gmail.com>
2025-06-30 20:54:23 +0100

    Upload Full Spellbook as an Item to [Add
    [Add SpellbookFull

---
+++ Scripts/Custom/FullSpellbookItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Server.Items
+{
+	public class SpellbookFull : Spellbook
+	{
+		[Constructable]
+		public SpellbookFull()
+		{
+			this.Content = ulong.MaxValue;
+		}
+		
+		public SpellbookFull( Serial serial ) : base( serial )
+		{
+		}
+		
+		public override void Serialize( GenericWriter writer )
+		{
+			base.Serialize( writer );
+			
+			writer.Write( (int) 0 );
+		}
+		
+		public override void Deserialize( GenericReader reader )
+		{
+			base.Deserialize( reader );
+			
+			int version = reader.ReadInt();
+		}
+	}
+}
