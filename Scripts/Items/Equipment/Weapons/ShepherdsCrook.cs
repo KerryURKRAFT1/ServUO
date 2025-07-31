ffbea241fc68d421171aa4c5abee4eed6110e085
Sirocco1106 <sirocco1106@gmail.com>
2025-07-31 15:13:12 +0100

    Skill Mod

--- Scripts/Items/Equipment/Weapons/ShepherdsCrook.cs
+++ Scripts/Items/Equipment/Weapons/ShepherdsCrook.cs
@@ -3,6 +3,7 @@ using Server.Engines.CannedEvil;
 using Server.Mobiles;
 using Server.Network;
 using Server.Targeting;
+using Server.SkillHandlers;
 
 namespace Server.Items
 {
@@ -132,101 +133,148 @@ namespace Server.Items
                 this.Weight = 4.0;
         }
 
+        public override void OnDoubleClick(Mobile m)
+        {
+        	if (IsChildOf(m.Backpack))
+        	{
+        		ClickToEquip.OnDoubleClick(m, this);
+        	}   		
+			else if (!SkillRegistry.Contains(m))
+    		{
+	    		SkillRegistry.Add(m);
+	        	
+	        	if (!TriggerSkill(m))
+	        	{
+		    		SkillRegistry.Remove(m);
+	        	}
+    		}
+    		else if (SkillRegistry.WaitMsg)
+    		{
+                m.SendMessage("You must wait to perform another action.");
+    		}
+        }
 
-            public override void OnDoubleClick(Mobile from)
-            {
-                if (from.FindItemOnLayer(this.Layer) == this)
-                {
-                from.SendLocalizedMessage(502464); // Target the animal you wish to herd.
-                from.Target = new HerdingTarget();
-                }
-                else
-                {
-                    ClickToEquip.OnDoubleClick(from, this);
-                }
-            }
+        public static bool TriggerSkill(Mobile from)
+        {
+            from.SendLocalizedMessage(502464); // Target the animal you wish to herd.
 
+            from.Target = new HerdingTarget();
+
+            return true;
+        }
 
         private class HerdingTarget : Target
         {
-            private static readonly Type[] m_ChampTamables = new Type[]
-            {
-                typeof(StrongMongbat), typeof(Imp), typeof(Scorpion), typeof(GiantSpider),
-                typeof(Snake), typeof(LavaLizard), typeof(Drake), typeof(Dragon),
-                typeof(Kirin), typeof(Unicorn), typeof(GiantRat), typeof(Slime),
-                typeof(DireWolf), typeof(HellHound), typeof(DeathwatchBeetle),
-                typeof(LesserHiryu), typeof(Hiryu)
-            };
-            public HerdingTarget()
-                : base(10, false, TargetFlags.None)
+            public HerdingTarget() : base(10, false, TargetFlags.None)
             {
             }
-
-
 
             protected override void OnTarget(Mobile from, object targ)
-            {
-                if (targ is BaseCreature)
-                {
-                    BaseCreature bc = (BaseCreature)targ;
+            {           	
+				new SkillTimer(from, targ, SkillRegistry.Delay).Start();
+            } 
 
-                    if (this.IsHerdable(bc))
-                    {
-                        if (bc.Controlled)
-                        {
-                            bc.PrivateOverheadMessage(MessageType.Regular, 0x3B2, 502467, from.NetState); // That animal looks tame already.
-                        }
-                        else
-                        {
-                            from.SendLocalizedMessage(502475); // Click where you wish the animal to go.
-                            from.Target = new InternalTarget(bc);
-                        }
-                    }
-                    else
-                    {
-                        from.SendLocalizedMessage(502468); // That is not a herdable animal.
-                    }
-                }
-                else
-                {
-                    from.SendLocalizedMessage(502472); // You don't seem to be able to persuade that to move.
-                }
+           protected override void OnTargetCancel(Mobile from, TargetCancelType cancelType)
+            {
+                SkillRegistry.Remove(from);
             }
 
-
-
-
-
-            private bool IsHerdable(BaseCreature bc)
+            protected override void OnTargetOutOfRange(Mobile from, object targeted)
             {
-                if (bc.IsParagon)
-                    return false;
-
-                if (bc.Tamable)
-                    return true;
-
-                Map map = bc.Map;
-
-                ChampionSpawnRegion region = Region.Find(bc.Home, map) as ChampionSpawnRegion;
-
-                if (region != null)
-                {
-                    ChampionSpawn spawn = region.ChampionSpawn;
-
-                    if (spawn != null && spawn.IsChampionSpawn(bc))
-                    {
-                        Type t = bc.GetType();
-
-                        foreach (Type type in m_ChampTamables)
-                            if (type == t)
-                                return true;
-                    }
-                }
-
-                return false;
+				from.LocalOverheadMessage(MessageType.Regular, 0x3B2, 1076203); // Target out of range.	
+				SkillRegistry.Remove(from);
             }
 
-            private class InternalTarget : Target
+	        protected override void OnTargetOutOfLOS(Mobile from, object o)
+	        {
+				from.LocalOverheadMessage(MessageType.Regular, 0x3B2, 500237);// Target can not be seen.
+                SkillRegistry.Remove(from);
+	        }
+
+	        private class SkillTimer : Timer
+			{
+				private readonly Mobile m_Owner;
+				private readonly object m_Targ;
+	
+	            public SkillTimer(Mobile owner, object targ, TimeSpan delay) : base(delay)
+				{
+					m_Owner = owner;
+					m_Targ = targ;
+	
+					Priority = TimerPriority.TwoFiftyMS;
+				}
+	
+				protected override void OnTick()
+				{
+	                if (m_Targ is BaseCreature)
+	                {
+	                    BaseCreature bc = (BaseCreature)m_Targ;
+	
+	                    if (this.IsHerdable(bc))
+	                    {
+	                        if (bc.Controlled)
+	                        {
+	                            bc.PrivateOverheadMessage(MessageType.Regular, 0x3B2, 502467, m_Owner.NetState); // That animal looks tame already.
+	                        }
+	                        else 
+	                        {
+	                            m_Owner.SendLocalizedMessage(502475); // Click where you wish the animal to go.
+	                            m_Owner.Target = new InternalTarget(bc);
+	                        }
+	                    }
+	                    else
+	                    {
+	                        m_Owner.SendLocalizedMessage(502468); // That is not a herdable animal.
+	                    }
+	                }
+	                else
+	                {
+	                    m_Owner.SendLocalizedMessage(502472); // You don't seem to be able to persuade that to move.
+	                }
+	                
+					SkillRegistry.Remove(m_Owner);
+				}
+
+	            private static readonly Type[] m_ChampTamables = new Type[]
+	            {
+	                typeof(StrongMongbat), typeof(Imp), typeof(Scorpion), typeof(GiantSpider),
+	                typeof(Snake), typeof(LavaLizard), typeof(Drake), typeof(Dragon),
+	                typeof(Kirin), typeof(Unicorn), typeof(GiantRat), typeof(Slime),
+	                typeof(DireWolf), typeof(HellHound), typeof(DeathwatchBeetle),
+	                typeof(LesserHiryu), typeof(Hiryu)
+	            };
+
+		        private bool IsHerdable(BaseCreature bc)
+	            {
+	                if (bc.IsParagon)
+	                    return false;
+	
+	                if (bc.Tamable)
+	                    return true;
+	
+	                Map map = bc.Map;
+	
+	                ChampionSpawnRegion region = Region.Find(bc.Home, map) as ChampionSpawnRegion;
+	
+	                if (region != null)
+	                {
+	                    ChampionSpawn spawn = region.ChampionSpawn;
+	
+	                    if (spawn != null && spawn.IsChampionSpawn(bc))
+	                    {
+	                        Type t = bc.GetType();
+	
+	                        foreach (Type type in m_ChampTamables)
+	                            if (type == t)
+	                                return true;
+	                    }
+	                }
+	
+	                return false;
+	            }
+	        }
+	        
+	        private class InternalTarget : Target
             {
                 private readonly BaseCreature m_Creature;
                 public InternalTarget(BaseCreature c)
